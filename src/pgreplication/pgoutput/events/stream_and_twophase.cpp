6bd9a5ab24414e49e7c7fcb9efa39ab3da6ca21f
Vladimir Voitenko <vladimirdev635@gmail.com>
2025-09-05 14:00:15 +0500

    fix

--- src/pgreplication/pgoutput/events/stream_and_twophase.cpp
+++ src/pgreplication/pgoutput/events/stream_and_twophase.cpp
@@ -1,12 +1,13 @@
 #include "./stream_and_twophase.hpp"
 
+#include <cstdint>
 #include <string>
 
 #include "pgreplication/utils.hpp"
 
 namespace PGREPLICATION_NAMESPACE::pgoutput::events {
 StreamPrepare StreamPrepare::fromBuffer(const input_buffer &buffer) {
-    return { .flags = buffer.subspan<0, 1>().front(),
+    return { .flags = static_cast<std::int8_t>(buffer.subspan<0, 1>().front()),
              .lsn = utils::int64FromNetwork(buffer.subspan<1, 8>()),
              .endLsn = utils::int64FromNetwork(buffer.subspan<9, 8>()),
              .timestamp = utils::int64FromNetwork(buffer.subspan<17, 8>()),
